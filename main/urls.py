f79d811879adc51c168313fdf712b8f45eb0060e
mursalatul <mursalatulislampallob@gmail.com>
2025-03-14 21:05:43 +0600

    basic setup for poll app

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 
 from main import views as mainviews
-from polls import views as pollviews
 
 urlpatterns = [
     path('',mainviews.main,name='main'),
--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from polls.models import *
+from polls.models import Question, Choice
 # Register your models here.
-
 admin.site.register([Question, Choice])
